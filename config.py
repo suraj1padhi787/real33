bc8187753c406b4ae9aef0d97fcb6a90a1f85ab6
suraj1padhi787 <suraj1padhi@gmail.com>
2025-04-05 18:22:10 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+API_ID = "5746709"
+API_HASH = "c00191beb5cdedc2bacf532977d07cd8"
+BOT_TOKEN = "8133172984:AAFJe0CTZynnI9bg5zUHdOAAavLXZkwagJQ"
+DB_PATH = "sessions.db"
+ADMIN_ID = 5893249491  # ← yahi tumhara Telegram user ID hona chahiye (as admin)
+
